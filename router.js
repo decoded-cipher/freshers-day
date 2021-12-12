d1a66755a43dbdc26dd4a5627ba069d2caf97abc
Arjun Krishna <arjun12345krishna@gmail.com>
2021-12-13 03:59:06 +0530

    Scrap

--- router.js
+++ router.js
@@ -1,20 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var db = require('./config/connection.js')
 
-// var data = require('./data.json')
-// var text = data[0];
+var data = require('./data.json')
 
 router.get('/', (req, res) => {
-    var txt = [];
-    db.query("SELECT * FROM students", (err, data) => {
-        if (!err) {
-            res.render('home', {data})
-            console.log(data);
-        } else {
-            throw err;
-        }
-    })
+    res.render('home', { data })
+    console.log(data);
 });
+
+router.get('/data', (req, res) => {
+    res.send({data});
+   });
 
 module.exports = router;
